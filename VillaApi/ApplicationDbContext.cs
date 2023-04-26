f79598894db23e90012fe40338991ce8fa981dc1
yennapu.abhilash <yennapu.abhilash@KELLGGNLPTP1572>
2023-04-26 23:20:50 +0530

    Add project files.

---
+++ VillaApi/ApplicationDbContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VillaApi.Models;
+
+namespace VillaApi
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Villa> Villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Villa>().HasData(
+                new Villa()
+                {
+                    Id = 1,
+                    Name = "Royal Villa",
+                    
+                    Occupancy = 8,
+                    
+                    Sqft = 1000,
+                   
+
+                },
+                new Villa
+                {
+                    Id = 2,
+                    Name = "killa Villa",
+                   
+                    Occupancy = 9,
+                    
+                    Sqft = 2000,
+                    
+
+                },
+            new Villa
+            {
+                Id = 3,
+                Name = "Raja Villa",
+               
+                Occupancy = 7,
+                
+                Sqft = 5000,
+                
+
+
+            });
+        }
+    }
+}
